ca3f28dbaf3bc9e9f5b47ffcc21c0b18dd66a941
Zhikrullah IGBALAJOBI <zalajobi@bask.health>
2023-06-05 22:06:01 +0100

    axios setup

---
+++ src/lib/axios.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: `${process.env.REACT_APP_HOST_PORT}${process.env.REACT_APP_BASE_URL}`,
+  timeout: 7500,
+})
+
+export const axiosGetRequest = async (url:string, params?:any) => {
+  const request = await instance.get(url, {
+    params
+  })
+
+  return request.data
+}
